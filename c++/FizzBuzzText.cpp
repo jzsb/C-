a82d91677f864ebb5b0d983d29e9c84971732f16
jzsb <jzsbyt@gmail.com>
2016-05-02 08:50:21 -0700

    C++ text

---
+++ c++/FizzBuzzText.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int number;
+  for (number = 1; number < 101; number++){
+  if(number % 3 == 0)
+    {cout<<"Fizz";}
+      cout<<"Buzz";
+      cout<<"FizzBuzz"<<endl;
+  }
+  return 0;
+}
